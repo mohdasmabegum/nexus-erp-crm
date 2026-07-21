690e527fe0f579047f1058ef7465769b126815ac
Mohammad Asma Begum <mdasma1304@gmail.com>
2026-07-21 23:40:20 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/health", (_, res) => {
+  res.json({
+    success: true,
+    message: "Nexus ERP CRM API Running"
+  });
+});
+
+export default app;
